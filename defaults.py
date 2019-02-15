3380291cbfd9bf0ca9dc0794e6c564e2239b63ea
wiese <wiesesamuel@gmail.com>
2019-02-15 13:24:07 +0100

    config

--- defaults.py
+++ defaults.py
@@ -37,12 +37,14 @@ ControllerConfig = {
     "stripe":  [[0, 2, 1, 3],
                 [4, 6, 5, 7],
                 [8, 10, 9, 11],
-                [12, 14, 13, -1],
+                [12, 14, 13, 15],
 
                 [1, 0, 2, 3], # duplicate
 
-                [25, 27, 26],
-                [20, 19, 21],
+                [16, 17, 18],
+                [19, 20, 21],
+                [22, 23, 24],
+                [25, 16, 27],
                 ],
     "color": [
         [0, 4, 8, 12, 20, 25],      # red
@@ -55,9 +57,14 @@ ControllerConfig = {
 pin_table_build_plan = {
     "head": ["PinsInUse", "color"],
     "color": ["red", "green", "blue", "white"],
-    "stripe": ["Leiste 1", "Leiste 2", "Leiste 3", "Leiste 4", "Leiste 5", "Tisch", "Bett", "Eck", "Schrank"]
-
+    "stripe": ["Leiste 1", "Leiste 2", "Leiste 3", "Leiste 4", "Leiste 5", "Tisch", "Bett", "Eck", "Schrank"],
+    "layout": {
+        "default_row": ["color"],
+        0: ["green", "red", "white", "blue"],
+        1: [],
+    }
 }
+
 
 Settings = {
     "verbose": 0,
